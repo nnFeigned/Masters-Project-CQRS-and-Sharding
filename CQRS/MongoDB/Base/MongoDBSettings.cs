3f4fd2c9372b6629168cba708724ca34526cd54d
nnFeigned <86807982+nnFeigned@users.noreply.github.com>
2023-12-09 18:43:58 +0200

    Delete CQRS directory

--- CQRS/MongoDB/Base/MongoDBSettings.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace CQRS.MongoDB.Base
-{
-    public class MongoDBSettings
-    {
-        public string ConnectionString { get; set; } = String.Empty;
-        public string ProductDatabaseName { get; set; } = String.Empty;
-    }
-}
